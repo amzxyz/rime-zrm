00eca91781ff1995389b9c73648e18eb17f6deb0
amzxyz <129564993+amzxyz@users.noreply.github.com>
2024-08-01 20:41:15 -0500

    Update force_gc.lua

--- lua/force_gc.lua
+++ lua/force_gc.lua
@@ -1,19 +1,9 @@
 -- 暴力 GC
 -- 详情 https://github.com/hchunhui/librime-lua/issues/307
+-- collectgarbage()：默认调用，等同于 collectgarbage("collect")，触发完整的垃圾回收。
+-- collectgarbage("step")：执行垃圾回收的一小步。这个函数会返回一个布尔值，表示这一步是否完成了整个收集周期。
 -- 这样也不会导致卡顿，那就每次都调用一下吧，内存稳稳的
 function force_gc()
     -- collectgarbage()
     collectgarbage("step")
-end
-
--- 临时用的
-function debug_checker(input, env)
-    for cand in input:iter() do
-        yield(ShadowCandidate(
-            cand,
-            cand.type,
-            cand.text,
-            env.engine.context.input .. " - " .. env.engine.context:get_preedit().text .. " - " .. cand.preedit
-        ))
-    end
 end
