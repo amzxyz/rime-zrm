b019c7b3bb3615bb532962166e514740caae1f73
amzxyz <rcm@qq.com>
2025-02-03 22:09:25 +0800

    新年伊始万象更新[D[D[D

--- lua/en_spacer.lua
+++ lua/en_spacer.lua
@@ -1,5 +1,4 @@
--- 这个 Lua 给英文单词后面自动加一个空格 #496
-
+-- 在英文单词后添加空格的函数
 local function add_space_to_english_word(input)
     -- 匹配纯英文单词（don't 算纯英文单词）并在单词后添加空格
     input = input:gsub("(%a+'?%a*)", "%1 ")
@@ -10,8 +9,8 @@ end
 local function en_spacer(input, env)
     for cand in input:iter() do
         if cand.text:match("^[%a']+[%a']*$") then
-            -- 如果候选项是纯英文单词，则在输入后添加一个空格
-            cand = cand:to_shadow_candidate(cand.type, add_space_to_english_word(cand.text), cand.comment)
+            -- 如果候选项是纯英文单词，则在输入后添加一个空格，并去掉注释
+            cand = cand:to_shadow_candidate(cand.type, add_space_to_english_word(cand.text), "")
         end
         yield(cand)
     end
